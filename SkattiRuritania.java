88f18fa6608cc5d3a63963fa9c970a414f830314
theahat <thea.hatlevold@gmail.com>
2025-01-12 14:50:47 +0100

    Skattiruritania

--- SkattiRuritania.java
+++ SkattiRuritania.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 class SkattiRuritania {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        double inntekt = in.nextDouble();
-        if (inntekt<10000) {
-            System.out.println(inntekt*0.10);
-        }
-        else {
-            double overskudd = inntekt-10000;
-            System.out.println((10000*0.10)+(overskudd*0.30));
+        try (Scanner in = new Scanner(System.in)) {
+            double inntekt = in.nextDouble();
+            if (inntekt<10000) {
+                System.out.println(inntekt*0.10);
+            }
+            else {
+                double overskudd = inntekt-10000;
+                System.out.println((10000*0.10)+(overskudd*0.30));
+            }
         }
     }
 }
