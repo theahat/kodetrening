43a851166d31a573d1186cc6a024a420bd017144
theahat <thea.hatlevold@gmail.com>
2025-01-13 19:13:56 +0100

    Boolskeverdier

---
+++ Boolskeverdier.java
@@ -0,0 +1,15 @@
+class Boolskeverdier {
+    public static void main(String[] args) {
+        boolean sann=true;
+        boolean usann=false;
+        System.out.println("Sann "+sann);
+        System.out.println("Usann "+usann);
+
+        if (sann!=usann){
+            System.out.println("Første test slo til!");
+        }
+        else {
+            System.out.println("Andre test slo ikke til!");
+        }
+    }
+}
