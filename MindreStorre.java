1a6582e9046a97bd25d28947254e5723f8c9d4ca
theahat <thea.hatlevold@gmail.com>
2025-01-13 19:42:54 +0100

    mindrestorre

---
+++ MindreStorre.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class MindreStorre {
+    public static void main(String[] args) {
+        int tall;
+        Scanner tastatur = new Scanner(System.in);
+
+
+        while(true){
+            System.out.println("Tast inn et tall:");
+            tall = Integer.parseInt(tastatur.nextLine());
+            if(tall < 10) {
+                System.out.println("Tallet er under 10");
+            } else if(tall >= 10 && tall <= 20) {
+                System.out.println("Tallet er mellom 10 og 20");
+            } else {
+                System.out.println("Tallet er over 20");
+            }
+        }
+    }
+}
