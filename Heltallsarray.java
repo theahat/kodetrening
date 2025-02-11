9f0ae0bcd3c370da871ffa0b21776685dadcc6e4
theahat <thea.hatlevold@gmail.com>
2025-02-11 20:27:11 +0100

    deleted unused

--- Heltallsarray.java
+++ Heltallsarray.java
@@ -1,9 +1,4 @@
 class Heltallsarray {
-    private static int tall0 = 0;
-    private static int tall1 = 1;
-    private static int tall2 = 2;
-    private static int tall3 = 3;
-    private static int tall4 = 4;
     private static int[] numbers = new int[5];
     public static void main(String[] args) {
         for (int i=0;i<5;i++){
